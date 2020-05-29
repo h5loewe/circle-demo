d36d927ef37d2667ae53806fcfeaf67d448b3299
loewe0202 <loewe0202@163.com>
2020-05-29 17:01:30 +0800

    vue-demo

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production' ? true : false;
 
 module.exports = {
-  publicPath: isProd ? './circle-demo' : './',
-  outputDir: isProd ? './circle-demo' : './'
+  publicPath: isProd ? '/circle-demo' : '/'
 };
